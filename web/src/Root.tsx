1258dc93d7a2760c744e3eea71c23c1691af2d78
Taylor Hulsmans <thulsmans.1133@gmail.com>
2024-07-30 20:58:32 +0200

    about

--- web/src/Root.tsx
+++ web/src/Root.tsx
@@ -1,6 +1,8 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useParams } from 'react-router-dom'
 import { ConnectWallet } from './components/ConnectWallet'
+import { About } from "@/components/About"
 export const Root = () => {
+  const { thread, board } = useParams()
   return (
     <>
       <div style={{width: '80vw', display: 'flex', alignItems: 'center', height: '10px'}}>
@@ -16,7 +18,11 @@ export const Root = () => {
         <h1><Link to="/">Hash Chan</Link></h1>
         <ConnectWallet />
       </div>
-      <Outlet />
+      {(!board && !thread) ? (
+        <About />
+      ):(
+        <Outlet />
+      )}
     </>
   )
 }
---
+++ web/src/components/About.tsx
@@ -0,0 +1,18 @@
+
+
+export const About = () => {
+  return (
+    <div style={{
+        display: 'grid'
+      }}>
+      <h3>Welcome to Hashchan</h3>
+      <p> - an image board that hides in blockchain event logs</p><br/>
+      <p> - There is no Central Server to DDos</p><br/>
+      <p> - You can run the client locally if this site is taken down</p><br/>
+      <p> - images piggyback on existing image hosting services</p><br/>
+      <p> - more expensive to botnet swarm</p><br/>
+      <p> - botnet swarms trying will pad your ethereum bags</p><br/>
+      <p> - no profits or hidden fees: just gas</p><br/>
+  </div>
+  )
+}
