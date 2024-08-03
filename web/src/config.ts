59c23054e1363bcbedfa8fc2d2bf85a1de4b2100
Taylor Hulsmans <thulsmans.1133@gmail.com>
2024-08-03 11:28:43 +0200

    add metadata

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,13 +1,25 @@
 import { http, createConfig } from 'wagmi'
+
 import { sepolia, localhost, optimismSepolia } from 'wagmi/chains'
 import { unstable_connector } from '@wagmi/core'
 import { injected, walletConnect } from 'wagmi/connectors'
+
 //import { injected } from 'wagmi/connectors'
 console.log(import.meta.env.VITE_WALLETCONNECT_PROJECT_ID)
+
+
+const metadata = {
+    name: 'HashChan',
+    description: 'imageboard inside ethereum eventlogs',
+    url: 'hashchan.network', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+          
+}
+
 export const config = createConfig({
   chains: [localhost, sepolia, optimismSepolia ],
   connectors: [
-    walletConnect({projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID}),
+    walletConnect({projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID, metadata}),
   ],
   transports: {
     [localhost.id]: http(),
