e5d75000eb8a71c17fb387a19c8c08a8f0977bd9
Taylor Hulsmans <thulsmans.1133@gmail.com>
2024-08-04 20:07:46 +0200

    about hyphens

--- web/src/components/About.tsx
+++ web/src/components/About.tsx
@@ -12,27 +12,27 @@ export const About = () => {
         width: '600px'
         }}>
       <h3>Welcome to Hashchan</h3>
-        <p> - hidden in ethereum event logs</p><br/>
-        <p> - Central Serverless</p><br/>
-        <p> - Local Client enabled</p><br/>
-        <p> - piggyback on existing image hosters</p><br/>
-        <p> - botnets trying to swarm will pad your ethereum bags</p><br/>
-        <p> - no profits or hidden fees: just gas</p><br/>
+        <p> - hidden in ethereum event logs</p>
+        <p> - Central Serverless</p>
+        <p> - Local Client enabled</p>
+        <p> - piggyback on existing image hosters</p>
+        <p> - botnets trying to swarm will pad your ethereum bags</p>
+        <p> - no profits or hidden fees: just gas</p>
       </div>
       <div style={{
           width: '300px'
         }}>
         <h3>Supported Chains</h3>
-        <p>Mainnet</p>
-        <p>Optimism</p>
-        <p>Sepolia Testnet</p>
-        <p>Optimism Sepolia Testnet</p>
+        <p> - Mainnet</p>
+        <p> - Optimism</p>
+        <p> - Sepolia Testnet</p>
+        <p> - Optimism Sepolia Testnet</p>
       </div>
       <div>
         <h3>Getting the most out of hashchan</h3>
-        <p>Use your own websocket rpc</p>
-        <p>Even better, run your own archive node</p>
-        <p>use ipfs pinning services for images</p>
+        <p> - Use your own websocket rpc</p>
+        <p> - Even better, run your own archive node</p>
+        <p> - use ipfs pinning services for images</p>
       </div>
   </div>
   )
