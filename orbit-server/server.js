56f9a7f8a3d0424635e236f317e72ee809095f9d
polus-arcticus <thulsmans.1133@gmail.com>
2026-05-04 14:16:11 -0600

    utils

--- orbit-server/server.js
+++ orbit-server/server.js
@@ -97,16 +97,16 @@ const main = async () => {
   }
 
   helia.libp2p.handle('/hashchan/orbitdb/1.0.0', async ({ stream, connection }) => {
-    const lp = lpStream(stream)
-    await lp.write(new TextEncoder().encode(JSON.stringify({ orbitDbAddr: db.address.toString() })))
     try {
+      const lp = lpStream(stream)
+      await lp.write(new TextEncoder().encode(JSON.stringify({ orbitDbAddr: db.address.toString() })))
       const msg = await lp.read()
       const { ready } = JSON.parse(new TextDecoder().decode(msg.subarray()))
       if (ready) {
         console.log('peer ready', connection.remotePeer.toString())
       }
     } catch (e) {
-      console.log('handshake error', e.message)
+      console.error('handshake error', e)
     }
   })
 
