008df8f7a848767e6a19022b0f20223a1ccf6a32
polus-arcticus <thulsmans.1133@gmail.com>
2026-05-05 20:11:34 -0600

    initiate direct

--- orbit-server/server.js
+++ orbit-server/server.js
@@ -86,19 +86,6 @@ const main = async () => {
   )
   console.log('db addr', db.address.toString())
 
-  // Verify manifest block is in blockstore
-  const { CID } = await import('multiformats/cid')
-  const { base58btc } = await import('multiformats/bases/base58')
-  const manifestCidStr = db.address.toString().replace('/orbitdb/', '').split('/')[0]
-  const manifestCid = CID.parse(manifestCidStr, base58btc)
-  try {
-    let bytes
-    for await (const chunk of blockstore.get(manifestCid)) { bytes = chunk }
-    console.log('[server] manifest block in blockstore:', bytes?.byteLength, 'bytes')
-  } catch (e) {
-    console.log('[server] manifest block NOT in blockstore:', e.message)
-  }
-
   console.log('serverlistening on: ')
   helia.libp2p.getMultiaddrs().forEach((addr) => {
     console.log(addr.toString())
@@ -243,7 +230,8 @@ const main = async () => {
   })
 
   db.events.on('join', (peerId, heads) => {
-    console.log("join", peerId, heads)
+    console.log('user looking to join orbit b')
+    console.log("db:events:on:join", peerId, heads)
   })
 
   process.on('SIGINT', async () => {
